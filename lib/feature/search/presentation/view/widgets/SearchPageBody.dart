77c44278de7fa8ab602b573b688c0d14ad75c208
Mina Victor <minavictor655@gmail.com>
2023-09-16 14:27:07 +0300

    finsh search page

--- lib/feature/search/presentation/view/widgets/SearchPageBody.dart
+++ lib/feature/search/presentation/view/widgets/SearchPageBody.dart
@@ -1,5 +1,8 @@
+import 'package:bookapp/feature/home/presentation/view/widgets/bestselleritem.dart';
 import 'package:bookapp/feature/search/presentation/view/widgets/customtextfield.dart';
 import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/styles.dart';
 
 class SearchPageBody extends StatelessWidget {
   const SearchPageBody({super.key});
@@ -9,8 +12,43 @@ class SearchPageBody extends StatelessWidget {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 30),
       child: Column(
-        children: [CustomTextField()],
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            height: 50,
+          ),
+          CustomTextField(),
+          SizedBox(
+            height: 20,
+          ),
+          Text(
+            'Best Seller',
+            style: Styles.textStyle18,
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Expanded(child: ListViewSearchItem())
+        ],
       ),
+    );
+  }
+}
+
+class ListViewSearchItem extends StatelessWidget {
+  const ListViewSearchItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 20,
+      padding: EdgeInsets.zero,
+      itemBuilder: (context, index) {
+        return const Padding(
+          padding: EdgeInsets.symmetric(vertical: 10),
+          child: CustomBestSellerItem(),
+        );
+      },
     );
   }
 }
