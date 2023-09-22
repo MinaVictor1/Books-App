8edf7936c145ff5ec36f49fcfcd980828e0d0b8c
Mina Victor <minavictor655@gmail.com>
2023-09-22 21:52:18 +0300

    implement fetch Newest Books

--- lib/feature/home/data/Models/rebos/home_repo.dart
+++ lib/feature/home/data/Models/rebos/home_repo.dart
@@ -3,6 +3,6 @@ import 'package:bookapp/feature/home/data/Models/book_model/book_model.dart';
 import 'package:dartz/dartz.dart';
 
 abstract class HomeRepo {
-  Future<Either<Failures, BookModel>> fetchBestSellerBooks();
-  Future<Either<Failures, BookModel>> fetchFeaturedBooks();
+  Future<Either<Failures, List<BookModel>>> fetchNewestBooks();
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks();
 }
--- lib/feature/home/data/Models/rebos/home_repo_impl.dart
+++ lib/feature/home/data/Models/rebos/home_repo_impl.dart
@@ -1,17 +1,32 @@
 import 'package:bookapp/core/errors/failures.dart';
+import 'package:bookapp/core/utils/api_service.dart';
 import 'package:bookapp/feature/home/data/Models/book_model/book_model.dart';
 import 'package:bookapp/feature/home/data/Models/rebos/home_repo.dart';
 import 'package:dartz/dartz.dart';
 
 class Home_Repo_Impl implements HomeRepo {
+  final ApiServise apiServise;
+
+  Home_Repo_Impl({required this.apiServise});
+
   @override
-  Future<Either<Failures, BookModel>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failures, List<BookModel>>> fetchNewestBooks() async {
+    var data = await apiServise.get(
+        endPoint:
+            'volumes?Filtering=free-ebooks&q=subject:Programming&Sorting=newest');
+
+    //here in our API we make a model only for items
+    //for that we make it
+    //if we took all info API we did not make it
+    List<BookModel> books = [];
+    for (var item in data['items']) {
+      books.add(BookModel.fromJson(item));
+    }
+    return right(books);
   }
 
   @override
-  Future<Either<Failures, BookModel>> fetchFeaturedBooks() {
+  Future<Either<Failures, List<BookModel>>> fetchFeaturedBooks() {
     // TODO: implement fetchFeaturedBooks
     throw UnimplementedError();
   }
